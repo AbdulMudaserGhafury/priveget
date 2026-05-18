d3b5f88af68950d424a032be6ddf84f653252839
suleymanov-suleyman <s.suleymanov@protonmail.com>
2026-05-18 21:20:06 +0300

    test commit

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 $host = 'localhost';
 $dbname = 'priveget_db'; 
 $user = 'root';          
-$pass = 'kendi veritabani sifresi';              
+$pass = 'kendi veritabani sifresi'; //test
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
