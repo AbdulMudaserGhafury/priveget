ff019954d22645f88401b99008e9d39af9ba290d
suleymanov-suleyman <s.suleymanov@protonmail.com>
2026-05-19 22:56:18 +0000

    typo: Dockerfile

--- db.php
+++ db.php
@@ -1,11 +1,10 @@
 <?php
 $env=parse_ini_file(__DIR__.'/.env'); //.env dosyasini okuyoruz
 
-$host = 'localhost';
+$host = $env['HOST_NAME']; //hem vps'te hem de yerelde bilgisayarda sorun yasamadan db'ye baglanmak icin
 $dbname = 'priveget_db'; 
 $user = 'root';          
-//$pass = $env['DATABASE_PASSWORD']; //database sifresini kullaniyoruz
-$pass = 'suleyman45';
+$pass = $env['DATABASE_PASSWORD']; //database sifresini kullaniyoruz
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
