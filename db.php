ca46bb19d515c4fe889add26840976c70ae8203a
suleymanov-suleyman <s.suleymanov@protonmail.com>
2026-05-20 02:39:33 +0300

    fix: hostname fix

--- db.php
+++ db.php
@@ -1,13 +1,15 @@
 <?php
-$env=parse_ini_file(__DIR__.'/.env'); //.env dosyasini okuyoruz
+$env=@parse_ini_file(__DIR__.'/.env'); //.env dosyasini okuyoruz
 
-$host = $env['HOST_NAME']; //hem vps'te hem de yerelde bilgisayarda sorun yasamadan db'ye baglanmak icin
+$host = isset($env['HOST_NAME']) ? $env['HOST_NAME'] : (getenv('HOST_NAME') ?: 'db');
+
 $dbname = 'priveget_db'; 
 $user = 'root';          
 $pass = $env['DATABASE_PASSWORD']; //database sifresini kullaniyoruz
 
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $db->setAttribute(PDO::ATTR_TIMEOUT, 3);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die(json_encode(["status" => "error", "message" => "Veritabanı bağlantı hatası: " . $e->getMessage()]));
